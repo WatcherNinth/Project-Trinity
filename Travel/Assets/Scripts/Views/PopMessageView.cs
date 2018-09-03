d2b1e97177741372d7c600955d9acc0673dd8025
luckywei88 <30662426+luckywei88@users.noreply.github.com>
2018-09-03 20:16:13 +0800

    Push .gitignore

---
+++ Travel/Assets/Scripts/Views/PopMessageView.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using Lucky;
+using UnityEngine.UI;
+
+public class PopMessageView : BaseSceneEaseInOut
+{
+    public Text Title;
+    public Text Content;
+
+    private string title="";
+    private string content="";
+
+    protected override void InitUI()
+    {
+        base.InitUI();
+        Enter();
+    }
+
+    protected override void UpdateView()
+    {
+        base.UpdateView();
+        SetData();
+    }
+
+    private void SetData()
+    {
+        Title.text = title;
+        Content.text = content;
+    }
+    
+
+    public void SetMessages(string ttitle, string tcontent)
+    {
+        title = ttitle;
+        content = tcontent;
+        InvalidView();
+    }
+}
