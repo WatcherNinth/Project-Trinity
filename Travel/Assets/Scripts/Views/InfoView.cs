395e29dad821ede1d6bd5151b3806deed7d2bc2a
luckywei88 <30662426+luckywei88@users.noreply.github.com>
2018-09-06 21:58:40 +0800

    优化Prefab管理和加上通知消息

---
+++ Travel/Assets/Scripts/Views/InfoView.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using Lucky;
+
+public class InfoView : BaseSceneEaseInOut
+{
+    public Text info;
+
+    private string message;
+    public string Message
+    {
+        set
+        {
+            message = value;
+            InvalidView();
+        }
+    }
+
+    protected override void InitUI()
+    {
+        base.InitUI();
+        Enter();
+    }
+
+    protected override void UpdateView()
+    {
+        base.UpdateView();
+        if (!string.IsNullOrEmpty(message))
+            info.text = message;
+    }
+}
