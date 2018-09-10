cbc8d31a912455ddbc2656eb42d6205cd2c8e24b
BruceChen7 <bofengqiye7@gmail.com>
2018-09-10 17:24:00 +0800

    生成完整的火车数据

--- main.py
+++ main.py
@@ -11,15 +11,14 @@ import sqlite3
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
-sys_encoding = sys.getfilesystemencoding()
 
 init_time = datetime.datetime.strptime("1970-01-01 8:00:00", "%Y-%m-%d %H:%M:%S")
-print init_time
+# print init_time
 ts = time.mktime(init_time.timetuple())
 
 def InsertData(t):
     sql = "insert into routine(start_node, end_node, start_time, end_time, type, money, ticket_name) values(\""  + \
-    t.start_node + "\",\"" + t.end_node + "\"," + str(t.start_time) + "," + str(t.end_time) + ", " + str(0)  + ", " + str(2000) + ","  + "\"" \
+    t.start_node + "\",\"" + t.end_node + "\"," + str(t.start_time) + "," + str(t.end_time) + ", " + str(0)  + ", " + str(t.expense) + ","  + "\"" \
     + t.tickets_name +"\")"
     print sql
 
@@ -114,37 +113,71 @@ def read_excel():
                 
                 t = Tickets(start_node, end_node, tickets_name, start_time_int, end_time_int, expense)
                 InsertData(t)
-                #ticket_list.append(t)
+
 
             
-        # if i >= 2:
-        #     # 读取每张列表
-        #     sheet = ExcelFile.sheet_by_name(sheet_names_list[i])
-        #     nrows = sheet.nrows   
+        if i >= 2:
+            # 读取每张列表
+            sheet = ExcelFile.sheet_by_name(sheet_names_list[i])
+            nrows = sheet.nrows   
         
-        #     for j in range(nrows):
-        #         if j == 0:
-        #             continue
+            for j in range(nrows):
+                if j == 0:
+                    continue
 
-        #         tickets_name = "".join(sheet.row_values(j)[0].split())
-        #         print tickets_name
+                tickets_name = str(sheet.row_values(j)[0]).encode("utf-8").decode("gbk")
+
+                start_node = sheet.row_values(j)[1]
+
+                end_node = sheet.row_values(j)[2]
+
+                if sheet.cell(j, 3).ctype == 3:
+                    cell = sheet.cell_value(j, 3)
+                    start_time = xldate_as_tuple(cell, 0)
+                    new_time = init_time + datetime.timedelta(hours = start_time[3], minutes = start_time[4])
+                    start_time_int = int(time.mktime(new_time.timetuple()))
+                else:
+                    start_time = sheet.row_values(j)[3]
+                    start_time_split_result = start_time.split(' ')
+                    print tickets_name, " split, end time ", start_time_split_result[1]
+                    hour = int(start_time_split_result[1].split(":")[0]) + 12
+                    minute = int(start_time_split_result[1].split(":")[1])
+                    new_time = init_time + datetime.timedelta(hours = hour, minutes = minute)
+                    start_time_int = int(time.mktime(new_time.timetuple()))
+                    print start_time_int
+
+                # print "start_time ", start_time
+
+                if sheet.cell(j, 4).ctype == 3:
+                    cell = sheet.cell_value(j, 4)
+                    end_time = xldate_as_tuple(cell, 0)
+                    hour = end_time[3]
+
+                    if end_time[3] < start_time[3]:
+                        hour += 12
+
+                    new_end_time = init_time + datetime.timedelta(hours = hour, minutes = end_time[4])
+                    end_time_int = int(time.mktime(new_end_time.timetuple()))
+                else:
+                    end_time = sheet.row_values(j)[4]
+                    end_time_split_result = end_time.split(' ')
+                    print tickets_name, " split, end time ", end_time_split_result[1]
+                    hour = int(end_time_split_result[1].split(":")[0]) + 12
+                    minute = int(end_time_split_result[1].split(":")[1])
+                    new_end_time = init_time + datetime.timedelta(hours = hour, minutes = minute)
+                    end_time_int = int(time.mktime(new_end_time.timetuple()))
+                    print end_time_int
+
+                #end_time = sheet.row_values(j)[4]
+                # print end_time
+
+                expense = sheet.row_values(j)[5]
+
+                # expense = str(sheet.row_values(j)[5])
+                print start_node, end_node, tickets_name, start_time_int, end_time_int , expense
                 
-        #         start_node =sheet.row_values(j)[1]
-        #         print start_node
-
-        #         end_node = sheet.row_values(j)[2]
-        #         print end_node
-
-        #         start_time = sheet.row_values(j)[3]
-        #         print start_time
-
-        #         end_time = sheet.row_values(j)[4]
-        #         print end_time
-
-        #         expense = sheet.row_values(j)[5]
-
-        #         # expense = str(sheet.row_values(j)[5])
-        #         print start_node, end_node, tickets_name, start_time, end_time , expense
+                t = Tickets(start_node, end_node, tickets_name, start_time_int, end_time_int, expense)
+                InsertData(t)
 
 def main():
     read_excel()
